b027eae06d8cd72653d0b7671817b770dfcddb1f
Waleed9211 <hafiz.waleed@nephrolytics.ai>
2024-09-15 22:57:54 +0500

    qr code tab done

---
+++ components/Settings/QrCode.jsx
@@ -0,0 +1,125 @@
+"use client";
+import { Box, Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
+import {
+  qrcodeBox,
+  qrcodeBoxWrapper,
+  qrcodeWrapper,
+  qrcopyLink,
+  qrnote,
+} from "@/styles/DashboarStyling";
+import ButtonComp from "../ui/button";
+import DownloadSvg from "@/public/assets/svg/DownloadSvg";
+import ColorPicker from "../ui/ColorPicker/ColorPicker";
+
+const QrCode = () => {
+  const [isCopied, setIsCopied] = useState(false);
+  const [qrColor, setQrColor] = useState("#A874F2");
+  const [bgQrColor, setBgQrColor] = useState("#FFFFFF");
+  const qrCodeLink = "https://www.google.com/";
+  const handleCopy = () => {
+    navigator.clipboard.writeText(qrCodeLink).then(() => {
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    });
+  };
+  return (
+    <Box>
+      <Grid container spacing={2} mt={2}>
+        <Grid item xs={12} sm={12} md={6} sx={qrcodeWrapper}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "40px",
+              borderBottom: "1px solid #0000000f",
+              paddingBottom: "40px",
+            }}
+          >
+            <Box>
+              <Typography
+                fontSize={"14px"}
+                color={"#000000d9"}
+                marginBottom={"10px"}
+              >
+                QR Color
+              </Typography>
+              <ColorPicker selectedColor={qrColor} onColorChange={setQrColor} />
+            </Box>
+            <Box sx={{ marginLeft: "15px" }}>
+              <Typography
+                fontSize={"14px"}
+                color={"#000000d9"}
+                marginBottom={"10px"}
+              >
+                Background Color
+              </Typography>
+              <ColorPicker
+                selectedColor={bgQrColor}
+                onColorChange={setBgQrColor}
+              />
+            </Box>
+          </Box>
+          <Box>
+            <Typography fontSize="16px" fontWeight={600} color="#000000d9">
+              QR Code for Menu Display
+            </Typography>
+            <Box sx={qrcodeBoxWrapper}>
+              <Box sx={qrcodeBox}>
+                <QRCode
+                  size={150}
+                  bgColor={bgQrColor}
+                  fgColor={qrColor}
+                  style={{
+                    height: "auto",
+                    maxWidth: "100%",
+                    width: "100%",
+                  }}
+                  value={"https://www.google.com/"}
+                  viewBox={`0 0 256 256`}
+                />
+              </Box>
+              <Box sx={qrnote}>
+                <Typography fontSize="14px" color="#000000d9">
+                  QR Code is very hard to read. Please increase the contrast by
+                  changing the colors.
+                </Typography>
+              </Box>
+
+              <Typography fontSize="14px" color="#000000d9" marginTop={"20px"}>
+                You can use this QR Code to display your Dine-in View Only Menu.
+              </Typography>
+              <Box sx={qrcopyLink}>
+                <ButtonComp
+                  text={"Download"}
+                  variant="purple"
+                  padding="4px 15px"
+                  startIcon={<DownloadSvg />}
+                />
+                <ButtonComp
+                  variant="transparent"
+                  onClick={handleCopy}
+                  text={"Copy QR Link"}
+                  marginLeft="10px"
+                  fontSize="14px"
+                  padding="4px 15px"
+                  sx={{ textTransform: "none" }}
+                >
+                  Copy QR Link
+                </ButtonComp>
+                {isCopied && (
+                  <Typography fontSize="12px" color="green" marginLeft="10px">
+                    Copied!
+                  </Typography>
+                )}
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default QrCode;
---
+++ components/Settings/VenueInformation.jsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const VenueInformation = () => {
+  return;
+  <Grid container spacing={2}>
+    <Grid item xs={12} sm={12} md={6}></Grid>
+  </Grid>;
+};
+
+export default VenueInformation;
